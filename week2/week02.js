d7acb58cd11e1a0a70a1f178fdda7ec2adcc57da
EC2 Default User <ec2-user@ip-172-31-88-90.ec2.internal>
2019-09-08 19:23:53 +0000

    updates on week02.js

--- week2/week02.js
+++ week2/week02.js
@@ -1,15 +1,18 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 
+// load the m09.text file into `content`, then load the `content` into a cheerio object
 var content = fs.readFileSync('../week1/m09.txt');
 var $ = cheerio.load(content);
-var meetingAddress=[];
 
+// create a container variable 'meetingAddress'
+var meetingAddress=[];
 
 $('td').each(function(i, ele) {
     if($(ele).attr("style")==='border-bottom:1px solid #e3e3e3; width:260px'){
         meetingAddress.push(($(ele).text().trim('')+"\n\n").replace(/\t/g,'').replace("\n      ","\n"))
     };
+    //Remove the extra information that's contained inside <span> tag and .detailsBox class
     $('span').replaceWith(function(i, ele) {
         return ''
     });
