f8b225ba665d31b860e7ea8bb50dc74fe04cf0c2
EC2 Default User <ec2-user@ip-172-31-88-90.ec2.internal>
2019-10-09 15:16:41 +0000

    updates

--- week4/week04_makeCSV.js
+++ week4/week04_makeCSV.js
@@ -4,7 +4,7 @@ function makeCSV(jsonFile,writeFile){
     var meetings = JSON.parse(jsonFile);
     
     // Wipe text file and add headings
-    fs.writeFileSync(writeFile, "LocationTitle,AddressStreetInfo,City,State,Zipcode,Latitude,Longitude,GroupTitle,Day,Start,End,MeetingType\n");
+    fs.writeFileSync('data/AAAll.csv', "LocationTitle,AddressStreetInfo,City,State,Zipcode,Details,Latitude,Longitude,GroupTitle,Day,Start,End,Meeting_Type\n");
     
     for (var locationName in meetings) {
         if (meetings.hasOwnProperty(locationName)) {
@@ -14,16 +14,15 @@ function makeCSV(jsonFile,writeFile){
                 for (let i = 0; i < meetings[locationName]['meetings'][meetingName].length; i++) { 
                                     
                     var saveString = 
-                        locationName + ',' +
+                        '"' + locationName + '"' + ',' +
                         meetings[locationName]['address']['street_info'] + ',' +
                         meetings[locationName]['address']['city'] + ',' +
                         meetings[locationName]['address']['state'] + ',' +
                         meetings[locationName]['address']['zip'] + ',' + '"' +
-                        // meetings[locationName]['address']['details'] + '"' + ',' +
+                        meetings[locationName]['address']['details'] + '"' + ',' +
                         meetings[locationName]['address']['coords']['latitude'] + ',' +
                         meetings[locationName]['address']['coords']['longitude'] + ',' +
                         meetings[locationName]['address']['zone'] + ',' +
-
                         '"' + meetingName + '"' + ',' +
                         meetings[locationName]['meetings'][meetingName][i]['day'] + ',' +
                         meetings[locationName]['meetings'][meetingName][i]['start'] + ',' +
@@ -31,7 +30,7 @@ function makeCSV(jsonFile,writeFile){
                         meetings[locationName]['meetings'][meetingName][i]['type'];
                     
                     // Save CSV into text file
-                    fs.appendFileSync(writeFile, saveString + '\n');
+                    fs.appendFileSync('data/AAAll.csv', saveString + '\n');
                 }
             }
         }
